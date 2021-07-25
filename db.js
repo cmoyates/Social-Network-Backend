1486b7c4ee38bfbebc47798d7b4aa357dc96e7dc
Cristopher Yates <cmoyates@gmail.com>
2021-07-25 19:45:19 -0230

    Attempting to enforce ssl

--- db.js
+++ db.js
@@ -10,7 +10,7 @@ const devConfig = {
 };
 
 const proConfig = {
-    connectionString: process.env.DATABASE_URL
+    connectionString: process.env.DATABASE_URL + "?ssl=true"
 };
 
 const pool = new Pool((process.env.NODE_ENV === "production") ? proConfig : devConfig);
