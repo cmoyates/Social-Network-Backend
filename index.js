8bb01f51b910701e2d802a61bd18f8f2a422cbf8
Cristopher Yates <cmoyates@gmail.com>
2021-07-25 19:21:14 -0230

    Added a message to show when running in production mode

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+if (process.env.NODE_ENV === "production") 
+{
+    console.log("Production!");
+}
+
 // Create
 app.post("/posts", async (req, res) => {    
     try {
